9a541fde6c47b7b11f3ec238043e6d53c0172169
Eunsong <151510169+Resong123456@users.noreply.github.com>
2024-02-06 23:32:00 +0900

    [Bronze III] Title: 배수와 약수, Time: 204 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -0,0 +1,33 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+
+			if (A != 0 && B != 0) {
+
+				if (B % A == 0) {
+					System.out.println("factor");
+
+				} else if (A % B == 0) {
+					System.out.println("multiple");
+
+				} else {
+					System.out.println("neither");
+
+				}
+				continue;
+
+			} else {
+				break;
+			}
+
+		}
+	}
+}
