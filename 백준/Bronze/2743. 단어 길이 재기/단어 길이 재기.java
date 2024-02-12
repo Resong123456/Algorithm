b5e772450b4dd3a9708ae89daa8665ab9121807f
Eunsong <151510169+Resong123456@users.noreply.github.com>
2024-02-13 02:00:17 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 204 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.nextLine();
+		
+		int[] arr = new int[str.length()];
+		
+		System.out.println(arr.length);
+        
+    }
+}
