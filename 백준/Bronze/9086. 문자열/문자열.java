f3071d89c741496bedb313b23b456296d301c704
Eunsong <151510169+Resong123456@users.noreply.github.com>
2024-02-17 03:08:31 +0900

    [Bronze V] Title: 문자열, Time: 232 ms, Memory: 18356 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,20 @@
+import java.awt.print.Printable;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int num = sc.nextInt();
+		sc.nextLine();
+
+		for (int i = 0; i < num; i++) {
+			String str = sc.nextLine();
+			System.out.println(str.charAt(0) + "" + str.charAt(str.length() - 1));
+
+		}
+
+	}
+}
