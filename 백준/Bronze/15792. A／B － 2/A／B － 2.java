665cbf1f3e02c2bf2863b8e9cd0d6e87632d4904
Eunsong <151510169+Resong123456@users.noreply.github.com>
2024-03-19 23:50:29 +0900

    [Bronze I] Title: A/B - 2, Time: 224 ms, Memory: 18084 KB, Score: 38 point -BaekjoonHub

--- 백준/Bronze/15792. A／B － 2/A／B － 2.java
+++ 백준/Bronze/15792. A／B － 2/A／B － 2.java
@@ -1,20 +1,18 @@
-import java.math.BigInteger;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Scanner;
-
-import javax.naming.ldap.HasControls;
 
 public class Main {
 
 	public static void main(String[] args) {
-
 		Scanner sc = new Scanner(System.in);
 
-		double A = sc.nextDouble();
-		double B = sc.nextDouble();
-
-		double C = A / B;
-
-		System.out.println(C);
+		BigDecimal A = sc.nextBigDecimal();
+		BigDecimal B = sc.nextBigDecimal();
+		
+		BigDecimal c = A.divide(B, 32, RoundingMode.HALF_UP).stripTrailingZeros();;
+		
+		System.out.println(c);
 
 	}
 }
